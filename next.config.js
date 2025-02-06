b4e881e573b5d1861c07ea802c59c2d165600c8a
崔家巍 <491758908@qq.com>
2025-02-06 11:00:15 +0800

    Update dependencies and configuration

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
   images: {
     unoptimized: true,
   },
+  basePath: "/cuijiawei123.github.io", // 替换 'repo-name' 为您的实际仓库名
 }
 
 module.exports = nextConfig
-
